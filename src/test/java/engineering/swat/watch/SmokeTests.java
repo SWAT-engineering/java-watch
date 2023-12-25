860ac759ca75c27ac7188d37b211870ffeb6798b
Davy Landman <davy.landman@swat.engineering>
2023-12-25 12:45:23 +0100

    Improved tests by removing sleeps

--- src/test/java/engineering/swat/watch/SmokeTests.java
+++ src/test/java/engineering/swat/watch/SmokeTests.java
@@ -11,11 +11,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import org.awaitility.Awaitility;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+
+import static org.awaitility.Awaitility.*;
+import static java.time.Duration.*;
+
 
 public class SmokeTests {
     static Path testDirectory;
@@ -24,8 +31,17 @@ public class SmokeTests {
     @BeforeAll
     static void setupTestDirectory() throws IOException {
         testDirectory = Files.createTempDirectory("smoke-test");
+        add3Files(testDirectory);
+        for (var d: Arrays.asList("d1", "d2", "d3")) {
+            Files.createDirectories(testDirectory.resolve(d));
+            add3Files(testDirectory.resolve(d));
+        }
+        Awaitility.setDefaultTimeout(1, TimeUnit.SECONDS);
+    }
+
+    private static void add3Files(Path root) throws IOException {
         for (var f : Arrays.asList("a.txt", "b.txt", "c.txt")) {
-            testFiles.add(Files.createFile(testDirectory.resolve(f)));
+            testFiles.add(Files.createFile(root.resolve(f)));
         }
     }
 
@@ -40,18 +56,35 @@ public class SmokeTests {
     }
 
     @Test
-    void watchSingleFile() throws IOException, InterruptedException {
+    void watchDirectory() throws IOException, InterruptedException {
         var changed = new AtomicBoolean(false);
         var target = testFiles.get(0);
-        var watchConfig = Watcher.singleDirectory(target.getParent())
+        var watchConfig = Watcher.singleDirectory(testDirectory)
             .onModified(p -> {if (p.equals(target)) { changed.set(true); }})
             ;
 
         try (var activeWatch = watchConfig.start() ) {
             Files.writeString(target, "Hello world");
-            Thread.sleep(1000);
-            assertTrue(changed.get(), "The file change should be detected");
+            await().alias("Target file change").until(changed::get);
         }
     }
+
+    @Test
+    void watchRecursiveDirectory() throws IOException, InterruptedException {
+        var changed = new AtomicBoolean(false);
+        var target = testFiles.stream()
+            .filter(p -> !p.getParent().equals(testDirectory))
+            .findFirst()
+            .orElseThrow();
+        var watchConfig = Watcher.recursiveDirectory(testDirectory)
+            .onModified(p -> {if (p.equals(target)) { changed.set(true); }})
+            ;
+
+        try (var activeWatch = watchConfig.start() ) {
+            Files.writeString(target, "Hello world");
+            await().alias("Nested file change").until(changed::get);
+        }
+    }
+
 
 }
