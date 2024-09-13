8cc2c39e735dbf94893bc2c7657264ab3c0eb21c
Davy Landman <davy.landman@swat.engineering>
2024-09-13 21:32:28 +0200

    Wrote javadocs

--- src/main/java/engineering/swat/watch/WatchEvent.java
+++ src/main/java/engineering/swat/watch/WatchEvent.java
@@ -4,10 +4,38 @@ import java.nio.file.Path;
 
 import org.checkerframework.checker.nullness.qual.Nullable;
 
+/**
+ * The library publishes these events to all subscribers, they are immutable and safe to share around.
+ */
 public class WatchEvent {
 
+    /**
+     * What happened with the file or directory
+     */
     public enum Kind {
-        CREATED, MODIFIED, DELETED, OVERFLOW
+        /**
+         *  A path entry was created. Be careful not to assume that when the event arrives, the path still exists.
+         **/
+        CREATED,
+        /**
+         * The path entry was saved. It is platform specific if this relates to flushes or other events.
+         *  a single user action can generate multiple of these events.
+         */
+        MODIFIED,
+        /**
+         * The path entry was deleted.
+         * Note that if the path entry was the watched item (aka the root of the watch),
+         * there is no guarantee if you will receive this event (depending on the level and on the platform).
+         * The watch will be invalid after that, even if a new item is created afterwards with the same name.
+         * In some cases this can be fixed/detected by also watching the parent, but that is only valid if they are on the same mountpoint.
+         */
+        DELETED,
+        /**
+         * Rare event where there were so many file events, that the kernel lost a few.
+         * In that case you'll have to consider the whole directory (and it's sub directories) as modified.
+         * The library will try and send events for new and deleted files, but it won't be able to detect modified files.
+         */
+        OVERFLOW
     }
 
     private final Kind kind;
@@ -24,14 +52,25 @@ public class WatchEvent {
         return this.kind;
     }
 
+    /**
+     *
+     * @return the path relative to the monitored root, it can be empty path if it's the root.
+     */
     public Path getRelativePath() {
         return relativePath;
     }
 
+    /**
+     *
+     * @return A copy of the root path that this event belongs to.
+     */
     public Path getRootPath() {
         return rootPath;
     }
 
+    /**
+     * @return utility function that resolves the relative path to the full path.
+     */
     public Path calculateFullPath() {
         return rootPath.resolve(relativePath);
     }
--- src/main/java/engineering/swat/watch/Watcher.java
+++ src/main/java/engineering/swat/watch/Watcher.java
@@ -2,6 +2,8 @@ package engineering.swat.watch;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
@@ -14,13 +16,20 @@ import engineering.swat.watch.impl.JDKDirectoryWatcher;
 import engineering.swat.watch.impl.JDKFileWatcher;
 import engineering.swat.watch.impl.JDKRecursiveDirectoryWatcher;
 
+/**
+ * Watch a path for changes.
+ *
+ * It will avoid common errors using the raw apis, and will try to use the most native api where possible.
+ * Note, there are differences per platform that cannot be avoided, please review the readme of the library.
+ */
 public class Watcher {
     private final Logger logger = LogManager.getLogger();
     private final WatcherKind kind;
     private final Path path;
     private Executor executor = CompletableFuture::runAsync;
 
-    private Consumer<WatchEvent> eventHandler = p -> {};
+    private static final Consumer<WatchEvent> NULL_HANDLER = p -> {};
+    private Consumer<WatchEvent> eventHandler = NULL_HANDLER;
 
 
     private Watcher(WatcherKind kind, Path path) {
@@ -35,38 +44,86 @@ public class Watcher {
         RECURSIVE_DIRECTORY
     }
 
-    public static Watcher singleFile(Path path) throws IOException {
+    /**
+     * Request a watcher for a single path (file or directory).
+     * If it's a file, depending on the platform this will watch the whole directory and filter the results, or only watch a single file.
+     * @param path a single path entry, either a file or a directory
+     * @return a watcher that only fires events related to the requested path
+     * @throws IOException in case the path is not absolute
+     */
+    public static Watcher single(Path path) throws IOException {
         if (!path.isAbsolute()) {
             throw new IOException("We can only watch absolute paths");
         }
         return new Watcher(WatcherKind.FILE, path);
     }
 
+    /**
+     * Request a watcher for a directory, getting events for its direct children.
+     * @param path a directory to monitor for changes
+     * @return a watcher that fires events for any of the direct children (and its self).
+     * @throws IOException in cas the path is not absolute or it's not an directory
+     */
     public static Watcher singleDirectory(Path path) throws IOException {
         if (!path.isAbsolute()) {
             throw new IOException("We can only watch absolute paths");
         }
+        if (!Files.isDirectory(path, LinkOption.NOFOLLOW_LINKS)) {
+            throw new IOException("Only directories are supported");
+        }
         return new Watcher(WatcherKind.DIRECTORY, path);
     }
 
+    /**
+     * Request a watcher for a directory, getting events for all of its children. Even those added after the watch started.
+     * On some platforms, this can be quite expansive, so be sure you want this.
+     * @param path a directory to monitor for changes
+     * @return a watcher that fires events for any of its children (and its self).
+     * @throws IOException in case the path is not absolute or it's not an directory
+     */
     public static Watcher recursiveDirectory(Path path) throws IOException {
         if (!path.isAbsolute()) {
             throw new IOException("We can only watch absolute paths");
         }
+        if (!Files.isDirectory(path, LinkOption.NOFOLLOW_LINKS)) {
+            throw new IOException("Only directories are supported");
+        }
         return new Watcher(WatcherKind.RECURSIVE_DIRECTORY, path);
     }
 
+    /**
+     * Callback that gets executed for every event. Can get called quite a bit, so be careful what happens here.
+     * Use the {@link #withExecutor(Executor)} function to influence the sequencing of these events.
+     * By default they can arrive in parallel.
+     * @param eventHandler a callback that handles the watch event, will be called once per event.
+     * @return this for optional method chaining
+     */
     public Watcher onEvent(Consumer<WatchEvent> eventHandler) {
         this.eventHandler = eventHandler;
         return this;
     }
 
+    /**
+     * Optionally configure the executor in which the {@link #onEvent(Consumer)} callbacks are scheduled.
+     * If not defined, every task will be scheduled on the {@link java.util.concurrent.ForkJoinPool#commonPool()}.
+     * @param callbackHandler worker pool to use
+     * @return this for optional method chaining
+     */
     public Watcher withExecutor(Executor callbackHandler) {
         this.executor = callbackHandler;
         return this;
     }
 
-    public Closeable start() throws IOException {
+    /**
+     * Start watch the path for events.
+     * @return a subscription for the watch, when closed, new events will stop being registered to the worker pool.
+     * @throws IOException in case the starting of the watcher caused an underlying IO exception
+     * @throws IllegalStateException the watchers is not configured correctly (for example, missing {@link #onEvent(Consumer)})
+     */
+    public Closeable start() throws IOException, IllegalStateException {
+        if (this.eventHandler == NULL_HANDLER) {
+            throw new IllegalStateException("There is no onEvent handler defined");
+        }
         switch (kind) {
             case DIRECTORY: {
                 var result = new JDKDirectoryWatcher(path, executor, this.eventHandler);
@@ -85,7 +142,7 @@ public class Watcher {
             }
 
             default:
-                throw new IllegalArgumentException("Not supported yet");
+                throw new IllegalStateException("Not supported yet");
         }
     }
 
