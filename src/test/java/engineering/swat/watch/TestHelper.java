38f9b9d31f241fec05792ac5138067e22fbbdcf3
Davy Landman <davy.landman@swat.engineering>
2024-09-18 18:32:29 +0200

    Increased timeouts on windows

--- src/test/java/engineering/swat/watch/TestHelper.java
+++ src/test/java/engineering/swat/watch/TestHelper.java
@@ -11,9 +11,16 @@ public class TestHelper {
     static {
         var delayFactorConfig = System.getenv("DELAY_FACTOR");
         int delayFactor = delayFactorConfig == null ? 1 : Integer.parseInt(delayFactorConfig);
-        if (System.getProperty("os", "?").toLowerCase().contains("mac")) {
+        var os = System.getProperty("os", "?").toLowerCase();
+        if (os.contains("mac")) {
             // OSX is SLOW on it's watches
             delayFactor *= 2;
+        }
+        else if (os.contains("win")) {
+            // windows watches can be slow to get everything
+            // published
+            // especially on small core systems
+            delayFactor *= 4;
         }
         SHORT_WAIT = Duration.ofSeconds(1 * delayFactor);
         NORMAL_WAIT = Duration.ofSeconds(4 * delayFactor);
