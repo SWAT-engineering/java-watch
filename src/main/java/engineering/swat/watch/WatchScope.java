fb01d397c0002e73f86b80420bb5d30db3fce3e1
Sung-Shik Jongmans <sung-shik.jongmans@swat.engineering>
2024-10-18 13:05:53 +0200

    Improve documentation of `PATH_ONLY` watch scopes

--- src/main/java/engineering/swat/watch/WatchScope.java
+++ src/main/java/engineering/swat/watch/WatchScope.java
@@ -5,7 +5,14 @@ package engineering.swat.watch;
  */
 public enum WatchScope {
     /**
-     *  Watch changes to a single file or (metadata of) a directory
+     * Watch changes to a single file or (metadata of) a directory, but not its
+     * content. That is, the following changes to a directory are watched:
+     *   - any modification caused by the creation of a nested file/directory;
+     *   - any modification caused by the deletion of a nested file/directory;
+     *   - any modification of its own metadata.
+     *
+     * When changes to nested files/directories should also be watched, use
+     * PATH_AND_CHILDREN or PATH_AND_ALL_DESCENDANTS.
      */
     PATH_ONLY,
     /**
