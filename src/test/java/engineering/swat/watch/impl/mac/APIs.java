9da18b50d112f6016119977bf22cdcdf64e58cf6
Sung-Shik Jongmans <sung-shik.jongmans@swat.engineering>
2025-04-21 15:53:40 +0200

    Update minimal working example with support for `USE_EXTENDED_DATA` to get inodes

--- src/test/java/engineering/swat/watch/impl/mac/APIs.java
+++ src/test/java/engineering/swat/watch/impl/mac/APIs.java
@@ -28,6 +28,8 @@ package engineering.swat.watch.impl.mac;
 
 import static engineering.swat.watch.impl.mac.apis.FileSystemEvents.FSEventStreamCreateFlag.FILE_EVENTS;
 import static engineering.swat.watch.impl.mac.apis.FileSystemEvents.FSEventStreamCreateFlag.NO_DEFER;
+import static engineering.swat.watch.impl.mac.apis.FileSystemEvents.FSEventStreamCreateFlag.USE_CF_TYPES;
+import static engineering.swat.watch.impl.mac.apis.FileSystemEvents.FSEventStreamCreateFlag.USE_EXTENDED_DATA;
 import static engineering.swat.watch.impl.mac.apis.FileSystemEvents.FSEventStreamCreateFlag.WATCH_ROOT;
 import static org.awaitility.Awaitility.await;
 
@@ -50,7 +52,9 @@ import com.sun.jna.Native;
 import com.sun.jna.Pointer;
 import com.sun.jna.platform.mac.CoreFoundation;
 import com.sun.jna.platform.mac.CoreFoundation.CFArrayRef;
+import com.sun.jna.platform.mac.CoreFoundation.CFDictionaryRef;
 import com.sun.jna.platform.mac.CoreFoundation.CFIndex;
+import com.sun.jna.platform.mac.CoreFoundation.CFNumberRef;
 import com.sun.jna.platform.mac.CoreFoundation.CFStringRef;
 
 import engineering.swat.watch.TestDirectory;
@@ -75,13 +79,13 @@ class APIs {
             var paths = ConcurrentHashMap.<String> newKeySet();
 
             var s = test.getTestDirectory().toString();
-            var handler = (MinimalWorkingExample.EventHandler) (path, flags, id) -> {
+            var handler = (MinimalWorkingExample.EventHandler) (path, inode, flags, id) -> {
                 synchronized (ready) {
                     while (!ready.get()) {
                         try {
                             ready.wait();
                         } catch (InterruptedException e) {
-                            LOGGER.error("Unexpected interrupt. Test likely to fail. Event ignored ({}).", prettyPrint(path, flags, id));
+                            LOGGER.error("Unexpected interrupt. Test likely to fail. Event ignored ({}).", prettyPrint(path, inode, flags, id));
                             Thread.currentThread().interrupt();
                             return;
                         }
@@ -90,7 +94,7 @@ class APIs {
                 paths.remove(path);
             };
 
-            try (var mwe = new MinimalWorkingExample(s, handler)) {
+            try (var mwe = new MinimalWorkingExample(s, handler, true)) {
                 var dir = test.getTestDirectory().toRealPath();
                 paths.add(Files.writeString(dir.resolve("a.txt"), "foo").toString());
                 paths.add(Files.writeString(dir.resolve("b.txt"), "bar").toString());
@@ -107,26 +111,27 @@ class APIs {
     }
 
     public static void main(String[] args) throws IOException {
-        var s = "/Users/sungshik/Desktop/tmp";
-        var handler = (MinimalWorkingExample.EventHandler) (path, flags, id) -> {
-            LOGGER.info(prettyPrint(path, flags, id));
+        var s = args[0];
+        var handler = (MinimalWorkingExample.EventHandler) (path, inode, flags, id) -> {
+            LOGGER.info(prettyPrint(path, inode, flags, id));
         };
+        var useExtendedData = args.length >= 2 && Boolean.parseBoolean(args[1]);
 
-        try (var mwe = new MinimalWorkingExample(s, handler)) {
+        try (var mwe = new MinimalWorkingExample(s, handler, useExtendedData)) {
             // Block the program from terminating until `ENTER` is pressed
             new BufferedReader(new InputStreamReader(System.in)).readLine();
         }
     }
 
-    private static String prettyPrint(String path, int flags, long id) {
+    private static String prettyPrint(String path, long inode, int flags, long id) {
         var flagsPrettyPrinted = Stream
             .of(FSEventStreamEventFlag.values())
             .filter(f -> (f.mask & flags) == f.mask)
             .map(Object::toString)
             .collect(Collectors.joining(", "));
 
-        var format = "path: \"%s\", flags: [%s], id: %s";
-        return String.format(format, path, flagsPrettyPrinted, id);
+        var format = "path: \"%s\", inode: %s, flags: [%s], id: %s";
+        return String.format(format, path, inode, flagsPrettyPrinted, id);
     }
 
     private static class MinimalWorkingExample implements Closeable {
@@ -134,7 +139,7 @@ class APIs {
         Pointer stream;
         Pointer queue;
 
-        public MinimalWorkingExample(String s, EventHandler handler) {
+        public MinimalWorkingExample(String s, EventHandler handler, boolean useExtendedData) {
 
             // Allocate singleton array of paths
             CFStringRef pathToWatch = CFStringRef.createCFString(s);
@@ -151,11 +156,24 @@ class APIs {
 
             // Allocate callback
             this.callback = (x1, x2, x3, x4, x5, x6) -> {
-                var paths = x4.getStringArray(0, (int) x3);
-                var flags = x5.getIntArray(0, (int) x3);
-                var ids = x6.getLongArray(0, (int) x3);
+                var paths  = x4.getStringArray(0, (int) x3);
+                var inodes = new long[(int) x3];
+                var flags  = x5.getIntArray(0, (int) x3);
+                var ids    = x6.getLongArray(0, (int) x3);
+
+                if (useExtendedData) {
+                    var extendedData = new CFArrayRef(x4);
+                    for (int i = 0; i < x3; i++) {
+                        var dictionary = new CFDictionaryRef(extendedData.getValueAtIndex(i));
+                        var dictionaryPath = dictionary.getValue(FileSystemEvents.kFSEventStreamEventExtendedDataPathKey);
+                        var dictionaryInode = dictionary.getValue(FileSystemEvents.kFSEventStreamEventExtendedFileIDKey);
+                        paths[i] = dictionaryPath == null ? null : new CFStringRef(dictionaryPath).stringValue();
+                        inodes[i] = dictionaryInode == null ? 0 : new CFNumberRef(dictionaryInode).longValue();
+                    }
+                }
+
                 for (int i = 0; i < x3; i++) {
-                    handler.handle(paths[i], flags[i], ids[i]);
+                    handler.handle(paths[i], inodes[i], flags[i], ids[i]);
                 }
             };
 
@@ -167,7 +185,8 @@ class APIs {
                 pathsToWatch,
                 FSE.FSEventsGetCurrentEventId(),
                 0.15,
-                NO_DEFER.mask | WATCH_ROOT.mask | FILE_EVENTS.mask);
+                NO_DEFER.mask | WATCH_ROOT.mask | FILE_EVENTS.mask |
+                    (useExtendedData ? USE_EXTENDED_DATA.mask | USE_CF_TYPES.mask : 0));
 
             // Deallocate array of paths
             pathsToWatch.release();
@@ -200,7 +219,7 @@ class APIs {
 
         @FunctionalInterface
         private static interface EventHandler {
-            void handle(String path, int flags, long id);
+            void handle(String path, long inode, int flags, long id);
         }
     }
 }
